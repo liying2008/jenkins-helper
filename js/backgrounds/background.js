f3bfcfe935c9ba2f091c2e7fd45f426a94113972
liying2008 <liruoer2008@yeah.net>
2019-07-07 00:12:24 +0800

    实现 stash params 功能

--- js/backgrounds/background.js
+++ js/backgrounds/background.js
@@ -1,6 +1,11 @@
 (function () {
   "use strict";
 
+  var CMD_STASH_PARAMS = "stash_params";
+  var CMD_APPLY_PARAMS = "apply_params";
+  var KEY_STASHED_PARAMS = "stashed_params";
+
+
   // NOTE:
   // 1、如果开启 Windows 10 系统的专注助手，则 Chrome 通知不会在屏幕右下角弹出，而是收在通知中心里面。
 
@@ -10,4 +15,15 @@
   });
   Services.start();
   NodeServices.start();
+
+  // 处理来自 content_scripts 的消息
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // console.log('request', request);
+    // console.log('sender', sender);
+    if (request.cmd === CMD_STASH_PARAMS) {
+      // 保存参数
+      localStorage.setItem(KEY_STASHED_PARAMS, JSON.stringify(request.data));
+      sendResponse({"code": 0});
+    }
+  });
 })();
--- js/contents/params_stash.js
+++ js/contents/params_stash.js
@@ -1,14 +1,198 @@
 (function () {
   "use strict";
 
+  var CMD_STASH_PARAMS = "stash_params";
+  var CMD_APPLY_PARAMS = "apply_params";
+
+  var PAGE_BUILD = 'build';
+  var PAGE_PARAMETERS = 'parameters';
+
+  // 获取文件内容
+  function readHtml(file, result, error) {
+    var url = chrome.runtime.getURL(file);
+    console.log('url', url);
+    fetch(url).then(function (res) {
+      if (res.ok) {
+        return res.text();
+      } else {
+        return Promise.reject(res);
+      }
+    }).then(result).catch(error)
+  }
+
+  // Build 页面 或 Rebuild页面
   function isBuildPage() {
+    // XPATH: //body[@id='jenkins']//div[@id='main-panel']/form/table[@class='parameters']
+    var selector = "body#jenkins div#main-panel form table.parameters";
+    var elements = $(selector);
+    console.log(elements);
+    if (elements.length === 1) {
+      return elements[0];
+    } else {
+      return null;
+    }
   }
 
-  function isRebuildPage() {
-
-  }
-
+  // 参数页面
   function isParametersPage() {
+    // XPATH: //body[@id='jenkins']//div[@id='main-panel']//table[@class='pane']
+    var selector = "body#jenkins div#main-panel table.pane";
+    var elements = $(selector);
+    console.log(elements);
+    if (elements.length === 1) {
+      return elements[0];
+    } else {
+      return null;
+    }
+  }
+
+
+  function getBuildPageParameters() {
+    // 暂存的参数
+    var stashedParams = {};
+    // 不能被暂存的参数
+    var cannotStashed = [];
+    var tBodies = $('tbody', table);
+    var size = tBodies.length;
+    for (var i = 0; i < size; i++) {
+      var parameter = $("tr td.setting-main div[name='parameter']", tBodies[i]);
+      var children = parameter.children();
+      if (children.length < 1) {
+        continue
+      } else if (children.length < 2) {
+        var c0 = children[0];
+        if (c0.name !== 'name') {
+          continue
+        }
+        // <input name="name" type="hidden" value="PARAM1">
+        console.log('getBuildPageParameters', c0.value + ' can not be stashed!');
+        cannotStashed.push(c0.value)
+      } else {
+        c0 = children[0];
+        var c1 = children[1];
+        if (c0.name !== 'name') {
+          continue
+        }
+        var pname = c0.value;
+        var type = c1.type;
+        var name = c1.name;
+        var value = c1.value;
+        console.log('param', 'PNAME=' + pname + ', tagName=' + c1.tagName + ', type=' + type + ', name=' + name + ', value=' + value);
+        // console.log('param', 'PNAME=' + pname + ', checked=' + c1.checked);
+
+        if (type === 'file' || type === 'hidden' || name === 'credentialType') {
+          // 无法暂存的参数类型
+          console.log('getBuildPageParameters', pname + ' can not be stashed!');
+          cannotStashed.push(pname);
+          continue
+        } else if (type === 'checkbox') {
+          // checkbox 特殊处理
+          value = c1.checked
+        }
+
+        stashedParams[pname] = {
+          'type': type,
+          'name': name,
+          'value': value,
+        }
+      } // end if
+    } // end for
+    return [stashedParams, cannotStashed]
+  }
+
+  function getParametersPageParameters() {
 
   }
+
+  function applyParameters() {
+
+  }
+
+  function getReadableParams(params) {
+    var str = '';
+    Object.keys(params).forEach(function (key) {
+      str += key + ': ' + params[key].value + '\n'
+    });
+    return str
+  }
+
+  function saveParameters(params) {
+    var message = {
+      "cmd": CMD_STASH_PARAMS,
+      "data": params,
+    };
+    chrome.runtime.sendMessage(message, function (resp) {
+      console.log('resp', resp);
+      if (resp.code === 0) {
+        console.log('saved.');
+        alert('Data saved successfully!\n\n' + getReadableParams(params))
+      } else {
+        console.log('error in saving.');
+        alert('Data saving failed!\n\n' + resp.message);
+      }
+    });
+  }
+
+  function addStashAndApplyButtons() {
+    var tBodies = $('tbody', table);
+    var size = tBodies.length;
+    if (size === 0) {
+      return;
+    }
+    var lastTBody = tBodies[size - 1];
+    var btnTd = $('tr td', lastTBody);
+    if (btnTd.length !== 1) {
+      return;
+    }
+    var htmlFile = "js/contents/params_stash_apply_btn.html";
+    readHtml(htmlFile, function (text) {
+      // add two buttons
+      btnTd.append(text);
+      // bind onClick listeners
+      bindEvent(table);
+    })
+  }
+
+  function bindEvent() {
+    $("#jenkins-helper-stash-parameters").on("click", function () {
+      var params = undefined;
+      if (currentPage === PAGE_BUILD) {
+        params = getBuildPageParameters()
+      } else if (currentPage === PAGE_PARAMETERS) {
+        params = getParametersPageParameters()
+      }
+      if (!params) {
+        console.log('获取参数失败');
+        alert('Parsing parameters failed!');
+        return
+      }
+      // 暂存的参数
+      var stashedParams = params[0];
+      // 不能被暂存的参数
+      var cannotStashed = params[1];
+      // save parameters to extension local storage
+      saveParameters(stashedParams);
+      // alert cannot stashed params
+      if (cannotStashed.length > 0) {
+        alert('Unsupported parameters: \n\n' + cannotStashed.join('\n'))
+      }
+    });
+    $("#jenkins-helper-apply-parameters").on("click", function () {
+      applyParameters()
+    });
+  }
+
+  var currentPage = PAGE_BUILD;
+  var table = isBuildPage();
+  if (table != null) {
+    currentPage = PAGE_BUILD;
+    addStashAndApplyButtons();
+  } else {
+    table = isParametersPage();
+    if (table != null) {
+      currentPage = PAGE_PARAMETERS;
+      addStashAndApplyButtons();
+    }
+  }
+
 })();
