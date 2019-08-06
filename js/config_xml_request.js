0442fa000cf6248b1eb03af8339fa1e7753db4d1
liying2008 <liruoer2008@yeah.net>
2019-08-06 23:59:29 +0800

    请求路径分为JenkinsURL和RequestPath

--- js/config_xml_request.js
+++ js/config_xml_request.js
@@ -2,13 +2,26 @@ new Vue({
   el: '#app',
   data: {
     strings: {},
-    requestUrl: '',
+    baseUrl: '',
+    requestPath: '',
     requestData: '',
     responseData: '',
     username: '',
     password: '',
   },
   computed: {
+    requestUrl: function () {
+      this.baseUrl = this.baseUrl.trim();
+      this.requestPath = this.requestPath.trim();
+      if (this.baseUrl === '') {
+        return ''
+      }
+      var tmpBaseUrl = this.baseUrl.charAt(this.baseUrl.length - 1) === '/' ?
+        this.baseUrl.substring(0, this.baseUrl.length - 1) : this.baseUrl;
+      var tmpRequestPath = this.requestPath.charAt(0) === '/' ?
+        this.requestPath.substring(1) : this.requestPath;
+      return tmpBaseUrl + '/' + tmpRequestPath
+    },
     btnGetDisabled: function () {
       return !this.requestUrl
     },
@@ -48,12 +61,45 @@ new Vue({
         _self.responseData = data;
       }).catch(function (e) {
         console.error("invokeGet 失败", _self.requestUrl, e);
+        _self.responseData = e.message || 'Failed to fetch.';
       });
     },
     invokePost() {
       var _self = this;
       // 先获取 crumb
+      this.getJenkinsCrumbValue(function (value, e) {
+        if (e != null) {
+          _self.responseData = e.message || 'Failed to fetch jenkins crumb.';
+          return
+        }
+        console.log('crumb value', value)
+      })
+    },
+    /**
+     * 获取 Jenkins crumb value
+     * @param callback
+     */
+    getJenkinsCrumbValue(callback) {
       // http://127.0.0.1:8080/jenkins/crumbIssuer/api/xml?xpath=//crumb
+      var _self = this;
+      var tmpBaseUrl = this.baseUrl.charAt(this.baseUrl.length - 1) === '/' ?
+        this.baseUrl.substring(0, this.baseUrl.length - 1) : this.baseUrl;
+      var url = tmpBaseUrl + '/crumbIssuer/api/xml?xpath=//crumb';
+      fetch(url, this.getRequestHeader()).then(function (res) {
+        if (res.ok) {
+          return res.text();
+        } else {
+          return Promise.reject(res);
+        }
+      }).then(function (data) {
+        var startIndex = data.indexOf('<crumb>');
+        var endIndex = data.indexOf('</crumb>');
+        var value = data.substring(startIndex + 7, endIndex);
+        callback(value, null)
+      }).catch(function (e) {
+        console.error("getJenkinsCrumbValue 失败", url, e);
+        callback(null, e)
+      });
     },
   }
 });
