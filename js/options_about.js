3396e685682ff8a219dd150e147a9463cce132d0
liying2008 <liruoer2008@yeah.net>
2019-11-24 20:18:35 +0800

    Write about page contents

--- js/options_about.js
+++ js/options_about.js
@@ -6,6 +6,9 @@ new Vue({
       settings: browser.i18n.getMessage("settings"),
       importAndExportSettings: browser.i18n.getMessage("importAndExportSettings"),
       about: browser.i18n.getMessage("about"),
+      sourceCode: browser.i18n.getMessage("sourceCode"),
+      contactAuthor: browser.i18n.getMessage("contactAuthor"),
+      reportIssue: browser.i18n.getMessage("reportIssue"),
     },
   },
 });
