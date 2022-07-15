7053e22cefc893ce6ad98ed53614f244ca9aa600
liying2008 <liruoer2008@yeah.net>
2022-07-15 23:39:11 +0800

    style(options): update about page style

--- src/options/pages/about/Index.vue
+++ src/options/pages/about/Index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { AtOutline, BugOutline, GitBranchOutline, KeyOutline, LogoGithub } from '@vicons/ionicons5'
 import { t } from '~/libs/extension'
 
 const strings = {
@@ -8,133 +9,104 @@ const strings = {
   reportIssue: t('reportIssue'),
 }
 
-const sourceCodeItems = [
-  {
-    icon: 'mdi-git',
-    link: 'https://github.com/liying2008/jenkins-helper',
-    text: 'https://github.com/liying2008/jenkins-helper',
-  },
-  {
-    icon: 'mdi-scale-balance',
-    link: 'https://github.com/liying2008/jenkins-helper/blob/master/LICENSE',
-    text: 'MIT',
-  },
+const aboutItems = [
+  [
+    {
+      icon: GitBranchOutline,
+      link: 'https://github.com/liying2008/jenkins-helper',
+      text: 'https://github.com/liying2008/jenkins-helper',
+    },
+    {
+      icon: KeyOutline,
+      link: 'https://github.com/liying2008/jenkins-helper/blob/master/LICENSE',
+      text: 'MIT',
+    },
+  ],
+  [
+    {
+      icon: AtOutline,
+      link: 'mailto:liruoer2008@yeah.net',
+      text: 'liruoer2008@yeah.net',
+      target: undefined,
+    },
+    {
+      icon: LogoGithub,
+      link: 'https://github.com/liying2008',
+      text: 'https://github.com/liying2008',
+      target: '_blank',
+    },
+  ], [
+    {
+      icon: BugOutline,
+      link: 'https://github.com/liying2008/jenkins-helper/issues',
+      text: 'https://github.com/liying2008/jenkins-helper/issues',
+    },
+  ],
 ]
 
-const contactAuthorItems = [
-  {
-    icon: 'mdi-email',
-    link: 'mailto:liruoer2008@yeah.net',
-    text: 'liruoer2008@yeah.net',
-    target: undefined,
-  },
-  {
-    icon: 'mdi-github',
-    link: 'https://github.com/liying2008',
-    text: 'https://github.com/liying2008',
-    target: '_blank',
-  },
-]
-
-const reportIssueItems = [
-  {
-    icon: 'mdi-bug',
-    link: 'https://github.com/liying2008/jenkins-helper/issues',
-    text: 'https://github.com/liying2008/jenkins-helper/issues',
-  },
+const aboutTitles = [
+  strings.sourceCode,
+  strings.contactAuthor,
+  strings.reportIssue,
 ]
 </script>
 
 <template>
-  <div id="options-about-wrapper">
-    <v-container>
-      <v-card
-        class="mx-auto mt-5"
-        max-width="900"
+  <n-el class="options-about-wrapper">
+    <n-card
+      v-for="(categoryItems, i) in aboutItems"
+      :key="i"
+      :title="aboutTitles[i]"
+      embedded
+      class="card"
+    >
+      <n-a
+        v-for="(item, j) in categoryItems"
+        :key="j"
+        class="item"
+        target="_blank"
+        :href="item.link"
       >
-        <v-container>
-          <v-card-title>{{ strings.about }}</v-card-title>
-          <!-- 项目源代码 -->
-          <v-list
-            subheader
-            shaped
-          >
-            <v-subheader>{{ strings.sourceCode }}</v-subheader>
-            <v-list-item
-              v-for="(item, i) in sourceCodeItems"
-              :key="i"
-              link
-              target="_blank"
-              :href="item.link"
-            >
-              <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-light">
-                  {{ item.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-
-          <v-divider></v-divider>
-
-          <!-- 联系作者 -->
-          <v-list
-            subheader
-            shaped
-          >
-            <v-subheader>{{ strings.contactAuthor }}</v-subheader>
-            <v-list-item
-              v-for="(item, i) in contactAuthorItems"
-              :key="i"
-              link
-              :target="item.target"
-              :href="item.link"
-            >
-              <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-light">
-                  {{ item.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-
-          <v-divider></v-divider>
-
-          <!-- 反馈问题 -->
-          <v-list
-            subheader
-            shaped
-          >
-            <v-subheader>{{ strings.reportIssue }}</v-subheader>
-            <v-list-item
-              v-for="(item, i) in reportIssueItems"
-              :key="i"
-              link
-              target="_blank"
-              :href="item.link"
-            >
-              <v-list-item-icon>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title class="font-weight-light">
-                  {{ item.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-container>
-      </v-card>
-      <div class="my-6" />
-    </v-container>
-  </div>
+        <n-icon
+          class="item-icon"
+          :component="item.icon"
+          size="22"
+        />
+        <span class="item-text">{{ item.text }}</span>
+      </n-a>
+    </n-card>
+  </n-el>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
+.options-about-wrapper {
+  padding: 24px;
+
+  .card {
+    max-width: 800px;
+    margin: auto;
+    margin-bottom: 10px;
+
+    .item {
+      display: flex;
+      align-items: center;
+      padding: 10px;
+
+      &:hover {
+        background-color: var(--hover-color);
+        border-radius: 6px;
+      }
+
+      .item-icon {
+        margin-right: 20px;
+        vertical-align: middle;
+      }
+
+      .item-text {
+        font-size: 16px;
+        font-weight: lighter;
+      }
+    }
+  }
+}
 </style>
