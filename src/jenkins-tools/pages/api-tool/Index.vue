25b78c73f6f5c3e22277e2c3cc7d96f8485ea874
liying2008 <liruoer2008@yeah.net>
2022-08-11 23:02:44 +0800

    chore(jenkins-tools): complete index component

--- src/jenkins-tools/pages/api-tool/Index.vue
+++ src/jenkins-tools/pages/api-tool/Index.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
+import type { SelectMixedOption } from 'naive-ui/es/select/src/interface'
 import PanelParams from './panel-params.vue'
 import PanelAuthorization from './panel-authorization.vue'
 import PanelHeaders from './panel-headers.vue'
 import PanelBody from './panel-body.vue'
 import type { Header } from './models'
 import { AuthorizationEntity } from './models'
-import { t } from '~/libs/extension'
 
 
 const HTTP_METHOD_GET = 'GET'
@@ -14,10 +14,24 @@ const HTTP_METHOD_POST = 'POST'
 const HTTP_METHOD_PUT = 'PUT'
 const HTTP_METHOD_DELETE = 'DELETE'
 
-const strings = {
-  close: t('close'),
-}
-const httpMethods = [HTTP_METHOD_GET, HTTP_METHOD_POST, HTTP_METHOD_PUT, HTTP_METHOD_DELETE]
+const httpMethods: SelectMixedOption[] = [
+  {
+    label: HTTP_METHOD_GET,
+    value: HTTP_METHOD_GET,
+  },
+  {
+    label: HTTP_METHOD_POST,
+    value: HTTP_METHOD_POST,
+  },
+  {
+    label: HTTP_METHOD_PUT,
+    value: HTTP_METHOD_PUT,
+  },
+  {
+    label: HTTP_METHOD_DELETE,
+    value: HTTP_METHOD_DELETE,
+  },
+]
 const httpMetod = HTTP_METHOD_GET
 const url = ref('')
 const configTab = ref('')
@@ -47,100 +61,70 @@ function send() {
 
 <template>
   <div id="api-tool-wrapper">
-    <v-container>
-      <div class="mx-auto mt-2">
-        <form>
-          <v-row align="center">
-            <div class="d-flex full-width">
-              <v-select
-                v-model="httpMetod"
-                dense
-                hide-details
-                :items="httpMethods"
-                label="Method"
-                solo
-                class="method-select ml-4"
-              ></v-select>
-
-              <v-text-field
-                v-model="url"
-                dense
-                label="Request URL"
-                placeholder="Enter request URL"
-                hide-details
-                outlined
-                class="full-width ml-6"
-              ></v-text-field>
-
-              <v-btn
-                class="text-capitalize mx-6"
-                :loading="false"
-                :disabled="false"
-                color="primary"
-                @click="send"
-              >
-                Send
-              </v-btn>
-            </div>
-          </v-row>
-          <v-row>
-            <v-tabs
-              v-model="configTab"
-              show-arrows
-              class="mt-4 mx-4"
-            >
-              <v-tab class="text-capitalize">
-                Query Params
-              </v-tab>
-              <v-tab class="text-capitalize">
-                Authorization
-              </v-tab>
-              <v-tab class="text-capitalize">
-                Headers
-              </v-tab>
-              <v-tab class="text-capitalize">
-                Body
-              </v-tab>
-            </v-tabs>
-            <v-tabs-slider></v-tabs-slider>
-            <v-tabs-items
-              v-model="configTab"
-              class="full-width"
-            >
-              <v-tab-item>
-                <PanelParams
-                  :url="url"
-                  @url-changed="urlChanged"
-                />
-              </v-tab-item>
-              <v-tab-item>
-                <PanelAuthorization
-                  :authorization-entity="authorizationEntity"
-                  @authorization-changed="authorizationChanged"
-                />
-              </v-tab-item>
-              <v-tab-item>
-                <PanelHeaders @headers-changed="headersChanged" />
-              </v-tab-item>
-              <v-tab-item>
-                <PanelBody />
-              </v-tab-item>
-            </v-tabs-items>
-          </v-row>
-        </form>
-      </div>
-    </v-container>
+    <div style="display: flex;">
+      <n-select
+        v-model:value="httpMetod"
+        :options="httpMethods"
+        style="width: 100px;"
+      />
+      <n-input
+        v-model:value="url"
+        :input-props="{ type: 'url' }"
+        placeholder="Enter request URL"
+        style="flex: 1;margin-right: 16px; margin-left: 8px;"
+      />
+      <n-button
+        type="primary"
+        :loading="false"
+        :disabled="false"
+        style="width: 100px;"
+        @click="send"
+      >
+        Send
+      </n-button>
+    </div>
+    <div style="margin-top: 10px;">
+      <n-tabs
+        v-model:value="configTab"
+        type="line"
+      >
+        <n-tab-pane
+          name="Query Params"
+          tab="Query Params"
+        >
+          <PanelParams
+            :url="url"
+            @url-changed="urlChanged"
+          />
+        </n-tab-pane>
+        <n-tab-pane
+          name="Authorization"
+          tab="Authorization"
+        >
+          <PanelAuthorization
+            :authorization-entity="authorizationEntity"
+            @authorization-changed="authorizationChanged"
+          />
+        </n-tab-pane>
+        <n-tab-pane
+          name="Headers"
+          tab="Headers"
+        >
+          <PanelHeaders @headers-changed="headersChanged" />
+        </n-tab-pane>
+        <n-tab-pane
+          name="Body"
+          tab="Body"
+        >
+          <PanelBody />
+        </n-tab-pane>
+      </n-tabs>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 #api-tool-wrapper {
-  .full-width {
-    width: 100%;
-  }
-
-  .method-select {
-    width: 158px;
-  }
+  padding: 24px;
 }
 </style>
