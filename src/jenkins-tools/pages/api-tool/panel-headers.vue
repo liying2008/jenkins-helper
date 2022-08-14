5ff942a6d494b6c9b15353b228b03ea57b6200f6
liying2008 <liruoer2008@yeah.net>
2022-08-14 17:47:07 +0800

    chore(jenkins-tools): update headers panel

--- src/jenkins-tools/pages/api-tool/panel-headers.vue
+++ src/jenkins-tools/pages/api-tool/panel-headers.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
-import { ref, watch } from 'vue'
+import { CloseCircleOutline } from '@vicons/ionicons5'
+import type { DataTableColumns } from 'naive-ui'
+import { NButton, NCheckbox, NIcon, NInput } from 'naive-ui'
+import { h, ref, watch } from 'vue'
 import { Header } from './models'
 
 // eslint-disable-next-line func-call-spacing
@@ -9,6 +12,68 @@ const emit = defineEmits<{
 
 const headers = ref<Header[]>([new Header()])
 
+const columns: DataTableColumns<Header> = [
+  {
+    title: '',
+    key: 'enabled',
+    render(row, index) {
+      return h(NCheckbox, {
+        checked: row.enabled,
+        class: 'parameter-enabled-cb',
+        onUpdateChecked(v) {
+          headers.value[index].enabled = v
+        },
+      })
+    },
+  },
+  {
+    title: 'Name',
+    key: 'name',
+    render(row, index) {
+      return h(NInput, {
+        value: row.name,
+        placeholder: 'Name',
+        disabled: !row.enabled && index !== headers.value.length - 1,
+        onUpdateValue(v) {
+          headers.value[index].name = v
+        },
+      })
+    },
+  },
+  {
+    title: 'Value',
+    key: 'value',
+    render(row, index) {
+      return h(NInput, {
+        value: row.value,
+        placeholder: 'Value',
+        disabled: !row.enabled && index !== headers.value.length - 1,
+        onUpdateValue(v) {
+          headers.value[index].value = v
+        },
+      })
+    },
+  },
+  {
+    title: 'Action',
+    key: 'actions',
+    render(row, index) {
+      if (index !== headers.value.length - 1) {
+        return h(
+          NButton,
+          {
+            text: true,
+            title: 'Remove this header',
+
+            class: 'parameter-remove-btn',
+            onClick: () => deleteHeader(index),
+          },
+          { default: () => h(NIcon, { component: CloseCircleOutline }) },
+        )
+      }
+    },
+  },
+]
 
 watch(headers, () => {
   console.log('0-headers', headers.value)
@@ -16,9 +81,9 @@ watch(headers, () => {
   const headersLength = headers.value.length
   // 判断最后一个条目是否为空，不为空则添加一个空条目
   const lastItem = headers.value[headersLength - 1]
-  if (lastItem.key !== '' || lastItem.value !== '') {
+  if (lastItem.name !== '' || lastItem.value !== '') {
     if (lastItem.initialState) {
-      headers.value[headersLength - 1].enable = true
+      headers.value[headersLength - 1].enabled = true
       headers.value[headersLength - 1].initialState = false
     }
     headers.value.push(new Header())
@@ -26,10 +91,10 @@ watch(headers, () => {
   console.log('1-headers', headers.value)
   // 根据当前Header列表生成实际使用的Header
   updateActualHeaders()
-})
+}, { deep: true })
 
 function updateActualHeaders() {
-  const actualHeaders = headers.value.filter((item: Header) => item.enable)
+  const actualHeaders = headers.value.filter((item: Header) => item.enabled)
   emit('headersChanged', actualHeaders)
 }
 
@@ -45,67 +110,10 @@ function deleteHeader(index: number) {
 
 <template>
   <div id="api-tool-panel-headers">
-    <v-container>
-      <v-row>
-        <v-col cols="3">
-          <div class="ml-8">
-            KEY
-          </div>
-        </v-col>
-        <v-col cols="8">
-          <div>VALUE</div>
-        </v-col>
-        <v-col cols="1">
-          <div></div>
-        </v-col>
-      </v-row>
-      <v-row
-        v-for="(header, index) in headers"
-        :key="index"
-        class="mt-2"
-      >
-        <v-col cols="3">
-          <div class="d-flex">
-            <v-checkbox
-              v-model="header.enable"
-              dense
-              hide-details
-              class="header-enable-cb"
-            ></v-checkbox>
-            <v-text-field
-              v-model="header.key"
-              dense
-              hide-details
-              :disabled="!header.enable && index !== headers.length - 1"
-              placeholder="Key"
-            ></v-text-field>
-          </div>
-        </v-col>
-        <v-col cols="8">
-          <v-text-field
-            v-model="header.value"
-            dense
-            hide-details
-            :disabled="!header.enable && index !== headers.length - 1"
-            placeholder="Value"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="1">
-          <v-btn
-            v-if="index !== headers.length - 1"
-            dense
-            icon
-            title="Remove this header"
-            x-small
-            class="header-remove-btn"
-            color="grey"
-            @click="deleteHeader(index)"
-          >
-            <v-icon>mdi-close-circle-outline</v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
+    <n-data-table
+      :columns="columns"
+      :data="headers"
+    />
   </div>
 </template>
 
