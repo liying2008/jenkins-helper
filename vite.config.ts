6132d0799747c53d05770a422d3d08a3e3dea879
liying2008 <liruoer2008@yeah.net>
2024-01-14 23:33:23 +0800

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,9 @@ export default defineConfig(({ mode }) => {
     },
     define: {
       __DEV__: isDev,
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
     },
     plugins: [
       vue(),
